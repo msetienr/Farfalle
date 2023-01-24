d7e993f878aee2ed51d117b352b9e98d4cf39449
Moncho R. Salgueiro <monchosalgueiro14@gmail.com>
2023-01-24 20:14:18 +0100

    menu navegacion

---
+++ src/Components/Navbar.js
@@ -0,0 +1,27 @@
+import { useRef } from "react";
+import { FaBars, FaTimes} from "react-icons/fa";
+import "../Styles/main.css";
+function Navbar() {
+    const navRef = useRef();    
+    const showNavbar = () => {
+        navRef.current.classList.toggle("responsive_nav");
+    }
+    return (
+        <header>
+            <h3>La Farfalle</h3>
+            <nav ref={navRef}>
+                <a href="/#">Inicio</a>
+                <a href="/#">Haz tu pedido</a>
+                <a href="/#">Reservar tu mesa</a>
+                <a href="/#">Iniciar sesión</a>
+                <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+                    <FaTimes/>
+                </button>
+            </nav>    
+            <button className="nav-btn" onClick={showNavbar}>
+                <FaBars />
+            </button>
+        </header>
+     );
+}
+export default Navbar;
